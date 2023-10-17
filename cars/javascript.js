8d381c01154e6474904f0f75284551f0788adff4
mikosavolainen <miko.markus2112@gmail.com>
2023-10-17 13:44:26 +0300

    More pages update

---
+++ cars/javascript.js
@@ -0,0 +1,20 @@
+function addToCart(productName, productPrice) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart.push({ name: productName, price: productPrice });
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+const searchInput = document.getElementById('search-input');
+
+searchInput.addEventListener('keyup', function() {
+    const query = this.value.toLowerCase();
+    const cars = document.querySelectorAll('.car');
+
+    cars.forEach(car => {
+        const carName = car.querySelector('h2').innerText.toLowerCase();
+        if (carName.includes(query)) {
+            car.style.display = 'block';
+        } else {
+            car.style.display = 'none';
+        }
+    });
+});
